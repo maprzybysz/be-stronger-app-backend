cfa1edc7b5321d94c3a24d705e84456a5be25078
Mateusz Przybysz <notentego678@gmail.com>
2020-12-14 18:23:51 +0100

    Init project

---
+++ src/main/java/pl/maprzybysz/bestrongerapp/BeStrongerAppApplication.java
@@ -0,0 +1,13 @@
+package pl.maprzybysz.bestrongerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeStrongerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeStrongerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/maprzybysz/bestrongerapp/BeStrongerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maprzybysz.bestrongerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeStrongerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
